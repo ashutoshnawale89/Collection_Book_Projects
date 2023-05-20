a052e9a6d1bf58fff1eb2b467b4e16117b045c24
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2023-05-20 21:43:43 +0530

    Added : Allow users to borrow a book from the library. Maintain a record of borrowed books

--- project/program/LibraryBook.py
+++ project/program/LibraryBook.py
@@ -1,21 +1,23 @@
+from project.program.Student import StudentUser
+
+
 class CollectionBook:
     # index 0 = Book Name   ,   index 1 = Book Author Name , index 2 = Book Publication year
     list_Book = []
-    def CollectionBook_init_(self):
-        print("Constructor call")
-
-    def default_AddingBooks(self):
-        self.list_Book.append({"Book Name": "The Christmas Pig", "Author Name": "JK Rowling", "Publication year": "2015"})
-        self.list_Book.append({"Book Name": "The Pig", "Author Name": "K Row", "Publication year": "2016"})
-        self.list_Book.append({"Book Name": "The Christmas", "Author Name": "J ling", "Publication year": "2017"})
-        self.list_Book.append({"Book Name": "The Cry Pig", "Author Name": "JK Row", "Publication year": "2018"})
-        self.list_Book.append({"Book Name": "Whereabouts", "Author Name": "Jhumpa Lahiri", "Publication year": "2020"})
+    borrow_Book =[]
+    def __init__(self):
+        self.list_Book.append({"Book Name": "The Christmas Pig", "Author Name": "JK Rowling", "Publication year": "2015", "Quantity": 25})
+        self.list_Book.append({"Book Name": "The Pig", "Author Name": "K Row", "Publication year": "2016", "Quantity": 25})
+        self.list_Book.append({"Book Name": "The Christmas", "Author Name": "J ling", "Publication year": "2017", "Quantity": 25})
+        self.list_Book.append({"Book Name": "The Cry Pig", "Author Name": "JK Row", "Publication year": "2018", "Quantity": 25})
+        self.list_Book.append({"Book Name": "Whereabouts", "Author Name": "Jhumpa Lahiri", "Publication year": "2020", "Quantity": 25})
 
     def addBook(self):
         book_Name = input("Enter Book Name: ")
         book_Author = input("Enter Book Author Name : ")
         book_Publication_Year = input("Enter Book Publication_year : ")
-        temp_list = {"Book Name": book_Name, "Author Name": book_Author, "Publication year": book_Publication_Year}
+        book_quantity = int(input("Enter the Qty of Books: "))
+        temp_list = {"Book Name": book_Name, "Author Name": book_Author, "Publication year": book_Publication_Year, "Quantity" : book_quantity}
         self.list_Book.append(temp_list)
 
     def seachByAuthorName(self, authorName):
@@ -32,10 +34,32 @@ class CollectionBook:
                 return
         print("Search By Book Name Data Not Found ..........!!  ")
 
+    def borrowBook(self,name,age,bookName):
+        for i in self.list_Book:
+            temp_bookName = ''
+            temp_authorName = ''
+            temp_publicationYear = ''
+            temp_qty = 0
+
+            if bookName == i["Book Name"]:
+                temp_bookName = i["Book Name"]
+                temp_authorName = i["Author Name"]
+                temp_publicationYear = i["Publication year"]
+                temp_qty = i["Quantity"] - 1
+                self.borrow_Book.append({"Name":name, "Age":age, "Book Name": temp_bookName, "Author Name": temp_authorName, "Publication year": temp_publicationYear, "Quantity" : 1})
+                self.list_Book.remove(i)
+                self.list_Book.append({"Book Name": temp_bookName, "Author Name": temp_authorName, "Publication year": temp_publicationYear, "Quantity" : temp_qty})
+                print("Book is successfully Borrowing......")
+                return
+        print("Search By Book Name Data Not Found ..........!!  ")
+
+
+
+
 
 
 obj = CollectionBook()
-obj.default_AddingBooks()
-print(obj.list_Book)
 obj.seachByAuthorName("K Row")
 obj.seachByBookName("The Pig")
+obj.borrowBook("Kunal Pandey",25,"The Pig")
+print(obj.borrow_Book)
---
+++ project/program/Student.py
@@ -0,0 +1,7 @@
+class StudentUser:
+    name = ""
+    id = ""
+    def __int__(self,name , id):
+        self.name = name
+        self.id =id
+        print("Student Constructor")
