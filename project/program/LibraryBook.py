5956e3d3ee9d9056bbf861c69780dd5153e6df3e
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2023-05-20 20:05:19 +0530

    Added : book to the library with details such as title, author, and publication year.

--- project/program/LibraryBook.py
+++ project/program/LibraryBook.py
@@ -1,15 +1,18 @@
 class CollectionBook:
 
-    list_book = []
+    list_Book = []
     def CollectionBook_init_(self):
-        self.list_book = self.list_book
+        self.list_Book = self.list_Book
         print("Constructor call")
 
     def addBook(self):
         book_Name = input("Enter Book Name: ")
-        self.list_book.append(book_Name)
+        book_Author = input("Enter Book Author Name : ")
+        book_Publication_Year = input("Enter Book Publication_year : ")
+        temp_list = [book_Name, book_Author, book_Publication_Year]
+        self.list_Book.append(temp_list)
 
 obj = CollectionBook()
 obj.addBook()
 obj.addBook()
-print(obj.list_book)
+print(obj.list_Book)
